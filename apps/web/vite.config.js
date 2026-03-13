1789d36508bedb4c59735ae175512a6dc27c9d6a
olivame <dyz@olivame.xyz>
2026-03-14 00:46:16 +0800

    Update web favicon and allow reverse proxy hosts

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -7,5 +7,10 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000,
+    allowedHosts: true,
+  },
+  preview: {
+    host: '0.0.0.0',
+    allowedHosts: true,
   },
 })
